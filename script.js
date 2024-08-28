119479d0dd98e601c175a474d423f8d10513115d
KRISH PATEL <kalpeshkrish.22.cse@iite.indusuni.ac.in>
2024-08-28 22:16:54 +0530

    finally done

--- script.js
+++ script.js
@@ -4,19 +4,36 @@ const nextButton = document.querySelector(".next-button");
 
 let slideIndex = 0;
 const slideCount = sliderInner.children.length;
+let autoSlideInterval;
 
 const updateSlider = () => {
   sliderInner.style.transform = `translateX(-${slideIndex * 100}%)`;
 };
 
+const startAutoSlide = () => {
+  autoSlideInterval = setInterval(() => {
+    slideIndex = (slideIndex + 1) % slideCount;
+    updateSlider();
+  }, 3000);
+};
+
+const stopAutoSlide = () => {
+  clearInterval(autoSlideInterval);
+};
+
 prevButton.addEventListener("click", () => {
+  stopAutoSlide();
   slideIndex = (slideIndex - 1 + slideCount) % slideCount;
   updateSlider();
+  startAutoSlide();
 });
 
 nextButton.addEventListener("click", () => {
+  stopAutoSlide();
   slideIndex = (slideIndex + 1) % slideCount;
   updateSlider();
+  startAutoSlide();
 });
 
 updateSlider();
+startAutoSlide();
